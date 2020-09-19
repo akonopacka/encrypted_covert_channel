e67c5be1f380a00f5ab5484e33e8a0f94ec55383
akonopacka <aleksandrakonopacka96@gmail.com>
2020-09-20 00:21:11 +0200

    working on server site

--- main.cpp
+++ main.cpp
@@ -12,23 +12,85 @@
 #include <unistd.h> // sleep()
 #include <iostream>
 #include "TCP_Packet.h"
+#include <stdio.h>
+#include <pcap.h>
+
+void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char*
+packet)
+{
+    static int count = 1;
+    fprintf(stdout, "%3d, ", count);
+    fflush(stdout);
+    count++;
+}
 
 int main(int argc, char **argv) {
     if (argc > 1) {
-        if (!strcmp(argv[1], "--server"))
+        if (!strcmp(argv[1], "--server")){
             std::cout << "Server!\n";
 
-        if (!strcmp(argv[1], "--client")) {
-            std::cout << "Client\n";
+        int i;
+        char *dev;
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t* descr;
+        const u_char *packet;
+        struct pcap_pkthdr hdr;
+        struct ether_header *eptr;    /* net/ethernet.h */
+        struct bpf_program fp;        /* hold compiled program */
+        bpf_u_int32 maskp;            /* subnet mask */
+        bpf_u_int32 netp;             /* ip */
 
-            TCP_Packet tcp_packet("192.168.1.18", "192.168.1.16", 1230, 1235);
-            // TODO zamienic send packet na zwracanie bool
-            std::cout << tcp_packet.send_packet();
-
-            // sleep for 1 seconds
-            sleep(1);
+        if(argc != 2){
+            fprintf(stdout, "Usage: %s \"expression\"\n"
+                    ,argv[0]);
             return 0;
         }
+
+        /* Now get a device */
+        dev = pcap_lookupdev(errbuf);
+
+        if(dev == NULL) {
+            fprintf(stderr, "%s\n", errbuf);
+            exit(1);
+        }
+        /* Get the network address and mask */
+        pcap_lookupnet(dev, &netp, &maskp, errbuf);
+
+        /* open device for reading in promiscuous mode */
+        descr = pcap_open_live(dev, BUFSIZ, 1,-1, errbuf);
+        if(descr == NULL) {
+            printf("pcap_open_live(): %s\n", errbuf);
+            exit(1);
+        }
+
+        /* Now we'll compile the filter expression*/
+        if(pcap_compile(descr, &fp, argv[1], 0, netp) == -1) {
+            fprintf(stderr, "Error calling pcap_compile\n");
+            exit(1);
+        }
+
+        /* set the filter */
+        if(pcap_setfilter(descr, &fp) == -1) {
+            fprintf(stderr, "Error setting filter\n");
+            exit(1);
+        }
+
+        /* loop for callback function */
+        pcap_loop(descr, -1, my_callback, NULL);
+    }
+    }
+
+
+    if (!strcmp(argv[1], "--client")) {
+        std::cout << "Client\n";
+
+        TCP_Packet tcp_packet("192.168.1.18", "192.168.1.16", 1230, 1235);
+        // TODO zamienic send packet na zwracanie bool
+        std::cout << tcp_packet.send_packet();
+
+        // sleep for 1 seconds
+        sleep(1);
+        return 0;
     } else {
         std::cerr << "Bad usage";
         return 1;
