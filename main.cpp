853849613bf10d4423e1121dc15fce41db05f047
akonopacka <aleksandrakonopacka96@gmail.com>
2020-10-06 21:43:09 +0200

    cleaning

--- main.cpp
+++ main.cpp
@@ -1,27 +1,17 @@
 /*
-	Raw TCP packets
+	Better description
 */
-#include <stdio.h>    //for printf
 #include <string.h> //memset
 #include <sys/socket.h>    //for socket of course
 #include <stdlib.h> //for exit(0);
-#include <errno.h> //For errno - the error number
 #include <netinet/tcp.h>    //Provides declarations for tcp header
 #include <netinet/ip.h>    //Provides declarations for ip header
 #include <arpa/inet.h> // inet_addr
 #include <unistd.h> // sleep()
 #include <iostream>
-#include "TCP_Packet.h"
-#include <stdio.h>
-#include <pcap.h>
-#include <iostream>
 #include <pcap.h>
 #include <net/ethernet.h>
-#include <netinet/ip.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <iostream>
 #include <tins/tins.h>
 
 using namespace Tins;
@@ -33,53 +23,13 @@ bool callback(const PDU &pdu) {
     // Find the TCP layer
     const TCP &tcp = pdu.rfind_pdu<TCP>();
     std::cout << ip.src_addr() << ':' << tcp.sport() << " -> "
-         << ip.dst_addr() << ':' << tcp.dport() << endl;
+              << ip.dst_addr() << ':' << tcp.dport() << endl;
     return true;
-}
-
-void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet){
-    const struct ether_header* ethernetHeader;
-    const struct ip* ipHeader;
-    const struct tcphdr* tcpHeader;
-    char sourceIp[INET_ADDRSTRLEN];
-    char destIp[INET_ADDRSTRLEN];
-    u_int sourcePort, destPort;
-    u_char *data;
-    int dataLength = 0;
-    std::string dataStr = "";
-
-    ethernetHeader = (struct ether_header*)packet;
-    if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) {
-        ipHeader = (struct ip *) (packet + sizeof(struct ether_header));
-        inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIp, INET_ADDRSTRLEN);
-        inet_ntop(AF_INET, &(ipHeader->ip_dst), destIp, INET_ADDRSTRLEN);
-
-        if (ipHeader->ip_p == IPPROTO_TCP) {
-            tcpHeader = (tcphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ip));
-            sourcePort = ntohs(tcpHeader->source);
-            destPort = ntohs(tcpHeader->dest);
-            data = (u_char *) (packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-            dataLength = pkthdr->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-
-            // convert non-printable characters, other than carriage return, line feed,
-            // or tab into periods when displayed.
-            for (int i = 0; i < dataLength; i++) {
-                if ((data[i] >= 32 && data[i] <= 126) || data[i] == 10 || data[i] == 11 || data[i] == 13) {
-                    dataStr += (char) data[i];
-                } else {
-                    dataStr += ".";
-                }
-            }
-
-            // print the results
-            std::cout << sourceIp << ":" << sourcePort << " -> " << destIp << ":" << destPort << std::endl;
-        }
-    }
 }
 
 int main(int argc, char **argv) {
     if (argc > 1) {
-        if (!strcmp(argv[1], "--server")){
+        if (!strcmp(argv[1], "--server")) {
             std::cout << "Server!\n";
 
             Sniffer("wlo1").sniff_loop(callback);
@@ -89,9 +39,10 @@ int main(int argc, char **argv) {
     if (!strcmp(argv[1], "--client")) {
         std::cout << "Client\n";
 
-        TCP_Packet tcp_packet("192.168.1.18", "192.168.1.16", 1239, 1235);
-        // TODO zamienic send packet na zwracanie bool
-        std::cout << tcp_packet.send_packet();
+        PacketSender sender;
+        IP pkt = IP("192.55.0.1") / TCP(22) / RawPDU("Martin is still cute");
+        sender.send(pkt);
+        std::cout << "Packet has been sent";
 
         // sleep for 1 seconds
         sleep(1);
