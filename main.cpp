7c9932612b72fb8f5dcc2b5759e1a59d9aaafdf2
akonopacka <aleksandrakonopacka96@gmail.com>
2020-10-17 22:49:12 +0200

    first working encryption

--- main.cpp
+++ main.cpp
@@ -1,25 +1,127 @@
 /*
 	Better description
 */
-#include <string.h> //memset
-#include <unistd.h> // sleep()
+#include <string.h>
+#include <unistd.h>
 #include <iostream>
 #include <tins/tins.h>
-#include <ctime>
 #include <chrono>
-#include <unistd.h>
 #include <thread>
 #include <bitset>
 #include <sstream>
+#include <cstdlib>
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <string>
+using std::string;
 
 using namespace Tins;
 using namespace std;
 
+
 string message = "";
-std::time_t start = std::time(nullptr);
 std::chrono::high_resolution_clock::time_point time_of_last_packet = std::chrono::high_resolution_clock::now();
 std::chrono::high_resolution_clock::time_point time_received;
 std::chrono::duration<double, std::milli> time_span;
+
+
+void handleErrors(void)
+{
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int ciphertext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
+
+    /*
+     * Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /*
+     * Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        handleErrors();
+    ciphertext_len = len;
+
+    /*
+     * Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        handleErrors();
+    ciphertext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
+
+    /*
+     * Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /*
+     * Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary.
+     */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
+
+    /*
+     * Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        handleErrors();
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
 
 bool callback_storage_CC(const PDU &pdu) {
     // Find the IP layer
@@ -76,7 +178,7 @@ bool callback_timing_CC(const PDU &pdu) {
 
             message = "";
         }
-        else if(interval < 200){
+        else if(interval < 1){
             cout<< "0"<<endl;
             message = message + "0";
         }
@@ -100,6 +202,50 @@ int main(int argc, char **argv) {
     }
 
     if (!strcmp(argv[1], "--client")) {
+
+
+        /* A 256 bit key */
+        unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+        /* A 128 bit IV */
+        unsigned char *iv = (unsigned char *)"0123456789012345";
+
+        /* Message to be encrypted */
+        unsigned char *plaintext =
+                (unsigned char *)"The quick brown fox jumps over the lazy dog";
+
+        /*
+         * Buffer for ciphertext. Ensure the buffer is long enough for the
+         * ciphertext which may be longer than the plaintext, depending on the
+         * algorithm and mode.
+         */
+        unsigned char ciphertext[128];
+
+        /* Buffer for the decrypted text */
+        unsigned char decryptedtext[128];
+
+        int decryptedtext_len, ciphertext_len;
+
+        /* Encrypt the plaintext */
+        ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+                                  ciphertext);
+
+        /* Do something useful with the ciphertext here */
+        printf("Ciphertext is:\n");
+        cout<<ciphertext<<endl;
+        BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+
+        /* Decrypt the ciphertext */
+        decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+                                    decryptedtext);
+
+        /* Add a NULL terminator. We are expecting printable text */
+        decryptedtext[decryptedtext_len] = '\0';
+
+        /* Show the decrypted text */
+        printf("Decrypted text is:\n");
+        printf("%s\n", decryptedtext);
+
         std::this_thread::sleep_for(std::chrono::milliseconds(5000));
         std::cout << "Client\n";
         string message = "HELLO1";
