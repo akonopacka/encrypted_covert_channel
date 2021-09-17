253f0fbc2e5ce0618d61d6948dbbe1db93820f19
ak <aleksandrakonopacka96@gmail.com>
2021-09-17 23:34:49 +0200

    testing script

--- test_client.sh
+++ test_client.sh
@@ -2,10 +2,19 @@
 
 echo "-------------------------------- Performing tests --------------------------------"
 
-# shellcheck disable=SC1066
+# Variables
 project_path="/home/ak/encrypted_covert_channel/cmake-build-debug"
+repeat_number=5
+
 echo "Path $project_path"
 cd $project_path
-./encrypted_covert_channel --client loss --is_encrypted des
+
+
+for i in $(seq 1 1 $repeat_number)
+do
+   echo ""
+   ./encrypted_covert_channel --client loss --is_encrypted des
+   sleep 2
+done
 
 echo "-------------------------------- Tests finished --------------------------------"
