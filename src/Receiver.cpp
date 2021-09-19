ede9f61d1befbe2c9c88cd6a0a302f7624a8f48c
ak <aleksandrakonopacka96@gmail.com>
2021-09-19 15:41:16 +0200

    saving results to one general file

--- src/Receiver.cpp
+++ src/Receiver.cpp
@@ -548,6 +548,10 @@ bool Receiver::loss_callback(const PDU &pdu) {
             results += "BER: " + std::to_string(BER) + "\n";
             std::cout << "Results: " << results << std::endl;
             Evaluation::save_results_to_file(results, Globals::results_path, "loss", "server");
+            std::string combined_results_path = "/home/ak/results/";
+            combined_results_path += "loss_" + Globals::cipher_type_+ ".csv";
+            std::ofstream log(combined_results_path, std::ios_base::app | std::ios_base::out);
+            log << std::to_string(BER)+";"+std::to_string(capacity)+";"+std::to_string(duration.count())+"\n";
 
             Globals::message_ = "";
             Globals::is_started_receiving = false;
--- test_client.sh
+++ test_client.sh
@@ -5,6 +5,8 @@ echo "-------------------------------- Performing tests ------------------------
 # Variables
 project_path="/home/ak/encrypted_covert_channel/cmake-build-debug"
 repeat_number=5
+covert_channel_method=loss
+cipher_method=des
 
 echo "Path $project_path"
 cd $project_path
@@ -13,7 +15,7 @@ cd $project_path
 for i in $(seq 1 1 $repeat_number)
 do
    echo ""
-   ./encrypted_covert_channel --client loss --is_encrypted des
+   ./encrypted_covert_channel --client $covert_channel_method --is_encrypted $cipher_method
    sleep 2
 done
 
