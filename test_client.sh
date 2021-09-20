6bfa2c023e704fbd8f3ea93f43d6462540a91eff
ak <aleksandrakonopacka96@gmail.com>
2021-09-20 17:31:53 +0200

    script working version

--- test_client.sh
+++ test_client.sh
@@ -4,7 +4,7 @@ echo "-------------------------------- Performing tests ------------------------
 
 # Variables
 project_path="/home/ak/encrypted_covert_channel/cmake-build-debug"
-repeat_number=1
+repeat_number=3
 ip_address=127.0.0.1
 
 echo "Path $project_path"
@@ -13,38 +13,44 @@ cd $project_path
 #printf "cd encrypted_covert_channel/cmake-build-debug; ./encrypted_covert_channel --server  loss --is_encrypted des \n" | nc  0.0.0.0 5000 &
 #./encrypted_covert_channel --server $covert_channel_method --is_encrypted $cipher_method
 #timing storage IP_id HTTP LSB sequence loss
-for covert_channel_type in storage IP_id HTTP LSB sequence loss
+for covert_channel_type in IP_id HTTP storage LSB sequence loss
 do
   cct=$covert_channel_type
-  echo "Testing covert channel type : $covert_channel_type"
-#  Test without encryption
-  printf "./encrypted_covert_channel --server  $cct \n" | nc  0.0.0.0 5000 &
-  echo "./encrypted_covert_channel --client $cct"
+  echo "Testing covert channel type : $cct "
+  #  Test without encryption
+  echo "sudo ./encrypted_covert_channel --server  $cct" | nc  0.0.0.0 5000 &
+  sleep 5
+  echo "Server starting for $cct"
+  #	echo "echo "./encrypted_covert_channel --server  $cct \n" | nc  0.0.0.0 5000 &"
+  #	echo "./encrypted_covert_channel --client $cct"
   for i in $(seq 1 1 $repeat_number)
   do
-     echo ""
-     ./encrypted_covert_channel --client $cct
-     sleep 2
+    echo "Repeat number $i"
+    sudo ./encrypted_covert_channel --client $cct
+    sleep 2
   done
-  sleep 5
+  sleep 10
   echo "sudo pkill -f encrypted_covert_channel" | nc  0.0.0.0 5000 &
   sleep 2
-    for cipher_type in aes des present rsa clefia grain
-    do
-      echo "Testing covert channel type : $covert_channel_type ; cipher: $cipher_type"
-      printf "./encrypted_covert_channel --server  $cct --is_encrypted $cipher_type \n" | nc  0.0.0.0 5000 &
-      sleep 2
-      echo "./encrypted_covert_channel --client $cct --is_encrypted"
-      for i in $(seq 1 1 $repeat_number)
-      do
-         echo ""
-         ./encrypted_covert_channel --client $cct --is_encrypted $cipher_type
-         sleep 2
-      done
-      sleep 5
-      echo "sudo pkill -f encrypted_covert_channel" | nc  0.0.0.0 5000 &
-      sleep 2
-    done
+#  echo "sudo pkill -f encrypted_covert_channel" | nc  0.0.0.0 5000 &
+#  sleep 2
+	
+#	for cipher_type in aes des present rsa clefia grain
+#	do
+#		echo "Testing covert channel type : $covert_channel_type ; cipher: $cipher_type"
+#		printf "./encrypted_covert_channel --server  $cct --is_encrypted $cipher_type \n" | nc  0.0.0.0 5000 &
+#		sleep 2
+#		echo "./encrypted_covert_channel --client $cct --is_encrypted"
+#		for i in $(seq 1 1 $repeat_number)
+#		do
+#			 echo ""
+#			 ./encrypted_covert_channel --client $cct --is_encrypted $cipher_type
+#		 sleep 2
+#	  done
+#	sleep 5
+#	echo "sudo pkill -f encrypted_covert_channel" | nc  0.0.0.0 5000 &
+#	sleep 2
+#	done
 done
 
 
